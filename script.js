ac4fe572a7d0399f9dcb43ec5e8533cf35ff9ee5
Kareem-124 <95405896+Kareem-124@users.noreply.github.com>
2023-04-12 16:26:27 +0300

    upload2

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function addLike(row){
     var like = document.querySelector(`.feedbackContainer:nth-child(${row}) .numberOfLikes`);
     like.innerHTML = parseInt(like.innerHTML)+1;
-    console.log(parseInt(like.innerHTML)+1);
 }
